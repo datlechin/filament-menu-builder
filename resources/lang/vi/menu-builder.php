7861ccefa498e6b51c7aa3628c0568a9f2435877
Ngo Quoc Dat <datlechin@gmail.com>
2024-08-06 20:23:17 +0700

    wip

---
+++ resources/lang/vi/menu-builder.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'form' => [
+        'title' => 'Tiêu đề',
+        'url' => 'URL',
+    ],
+    'actions' => [
+        'add' => [
+            'label' => 'Thêm vào Menu',
+        ],
+    ],
+    'items' => [
+        'empty' => [
+            'heading' => 'Không có mục nào trong menu này.',
+        ],
+    ],
+    'custom_link' => 'Liên kết Tùy chỉnh',
+    'open_in' => [
+        'label' => 'Mở trong',
+        'options' => [
+            'self' => 'Cùng tab',
+            'blank' => 'Tab mới',
+            'parent' => 'Tab cha',
+            'top' => 'Tab trên cùng',
+        ],
+    ],
+    'notifications' => [
+        'created' => [
+            'title' => 'Liên kết đã được tạo',
+        ],
+    ],
+];
