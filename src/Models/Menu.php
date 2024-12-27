a20767931ddc06a8d365d16420c2c2fa374412d1
datlechin <datlechin@users.noreply.github.com>
2024-12-27 04:21:24 +0000

    Fix styling

--- src/Models/Menu.php
+++ src/Models/Menu.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace Datlechin\FilamentMenuBuilder\Models;
 
 use Datlechin\FilamentMenuBuilder\FilamentMenuBuilderPlugin;
-use Illuminate\Contracts\Database\Query\Builder;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
